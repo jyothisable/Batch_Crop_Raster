2f7fec554303386674d04d8caf019d9a900e8839
athul jyothis <63841734+jyothisable@users.noreply.github.com>
2021-09-12 00:34:29 +0530

    vector input

--- app.py
+++ app.py
@@ -15,11 +15,18 @@ from datetime import datetime as d
 # change directory because the file is usually imported to grass gis
 os.chdir(os.path.dirname(__file__))
 
+clipRef = 'data/reference/clipRef.gpkg'
+gs.run_command('v.import', input=clipRef ,output='clipRef', overwrite=True)
+# limit computational region
+gs.run_command('r.mask' ,vect='clipRef')
 
 files = Path('data/input_raster').glob('*.tif')
 for file in files:
-    gs.run_command('r.in.gdal', input=file,
-                    output='DEM', overwrite=True)
-        
+    inputFileInfo = os.path.basename(file).split('_')
+    print(inputFileInfo)
+    date = inputFileInfo[1]
+    time = inputFileInfo[2]
+    # gs.run_command('r.in.gdal', input=file,output='input_raster', overwrite=True)
+    # gs.run_command('r.mapcalc' ,ex='clipped_raster = input_raster')
 
 
