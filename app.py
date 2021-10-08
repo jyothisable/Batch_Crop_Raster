a34f887efffb1b33c959b4e0fb28fbad94bb8e2e
Athul Jyothis <athuljyothis18@gmail.com>
2021-10-08 15:01:14 +0530

    more readable

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env conda run -n MTP python3
+#!/usr/bin/env python3
 # =============================================================================
 # Created By  : Athul Jyothis
 # Created Date: 11-09-2021 23:32:21
@@ -12,7 +12,7 @@ import csv
 import grass.script as gs
 from datetime import datetime as d
 # change directory because the file is usually imported to grass gis
-os.chdir(os.path.dirname(__file__))
+os.chdir('/home/jyothisable/P.A.R.A/1.Projects/mtp/Softwares/VS code/Batch_Stats_Raster')
 
 def saveOutput(inputFileName,fileNameInGrass):
     '''
@@ -21,7 +21,10 @@ def saveOutput(inputFileName,fileNameInGrass):
     '''
     # output results stats into CSV (can't append directly)
     gs.run_command('r.univar', map=fileNameInGrass,
-                    output='data/cache/stats_cache.csv', separator='comma', overwrite=True, flags='te')
+                    output='data/cache/stats_cache.csv', 
+                    separator='comma', 
+                    overwrite=True, 
+                    flags='te')
     # read the last lines from cache file
     with open('data/cache/stats_cache.csv', newline='') as cache_csv:
         lastLine = cache_csv.read().splitlines()[-1]
@@ -30,7 +33,7 @@ def saveOutput(inputFileName,fileNameInGrass):
         # save header initially
         if os.stat('data/output/stats.csv').st_size == 0:
             output_csv.writelines(
-                'day,time,non_null_cells,null_cells,min,max,range,mean,mean_of_abs,stddev,variance,coeff_var,sum,sum_abs,first_quart,median,third_quart,perc_90')
+                'day,time(UTC),non_null_cells,null_cells,min,max,range,mean,mean_of_abs,stddev,variance,coeff_var,sum,sum_abs,first_quart,median,third_quart,perc_90')
         output_csv.write("\n")
         output_csv.writelines(str(day)+','+str(time)+','+lastLine)
 
@@ -38,9 +41,9 @@ def saveOutput(inputFileName,fileNameInGrass):
 files = Path('data/input_raster').glob('*.tif')
 for file in files:
     inputFileInfo = os.path.basename(file).split('_')
-    date = inputFileInfo[2]
+    date = inputFileInfo[3]
     day = date[:2]
-    time = inputFileInfo[3]
+    time = inputFileInfo[4]
     gs.run_command('r.in.gdal', input=file,output='solar_ins', flags='o' ,overwrite=True)
     saveOutput(os.path.basename(file),'solar_ins')
 
